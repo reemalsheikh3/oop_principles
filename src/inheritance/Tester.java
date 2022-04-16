5a156260cfbd1b2289eebf413e84455471c6eea8
reemalsheikh3 <reemalsheikh08@gmail.com>
2022-04-16 15:41:00 -0500

    method overriding

--- src/inheritance/Tester.java
+++ src/inheritance/Tester.java
@@ -10,4 +10,23 @@ public class Tester extends Person{
     public void code(){
         System.out.println("Tester codes");
     }
+    @Override
+    public void eat(){
+        System.out.println("Tester eats");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Tester sleeps");
+    }
+
+    @Override
+    public void learn() {
+        System.out.println("Tester learns");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Tester walks");
+    }
 }
